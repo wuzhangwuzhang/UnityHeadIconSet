ea01acbe6ff0a117608daac2f8e482956fc1991d
wuzhang <930116897@qq.com>
2016-11-20 04:56:56 +0800

    玩家自定义圆角小头像

---
+++ Assets/Material/Mask.shader
@@ -0,0 +1,19 @@
+﻿Shader "Custom/Mask" {
+	Properties 
+	{
+		_MainTex ("Main Texture", 2D) = "white" {}
+		_Mask("Mask Texture",2D)="white"{}
+	}
+	SubShader 
+	{
+		Tags{"Queue"="Transparent"}
+		Lighting On
+		Zwrite off
+		Blend SrcAlpha OneMinusSrcAlpha
+		pass
+		{
+			SetTexture[_Mask]{combine texture}
+			SetTexture[_MainTex]{combine texture,previous}
+		}
+	}
+}
---
+++ Assets/Src/Framework/CloudFileUtil/Conf/Config.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace UFileCSharpSDK
+{
+	public class Config
+	{
+		public static string VERSION = @"1.0.2";
+
+		/// <summary>
+		/// UCloud管理服务器地址后缀
+		/// </summary>
+		public static string UCLOUD_PROXY_SUFFIX = @".ufile.ucloud.cn";
+
+		/// <summary>
+		/// UCloud提供的公钥
+		/// </summary>
+        public static string UCLOUD_PUBLIC_KEY = @"";
+
+		/// <summary>
+		/// UCloud提供的密钥
+		/// </summary>
+        public static string UCLOUD_PRIVATE_KEY = @"";
+
+
+		public static string GetUserAgent() 
+		{
+			return @"UCloudCSharp/" + VERSION;
+		}
+	}
+}
+
